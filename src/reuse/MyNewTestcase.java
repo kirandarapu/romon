11a731a4c6911738c84b8cbce23ef33c3379f597
kirandarapu <naveendarapu108@gmail.com>
2023-09-21 10:27:59 +0530

    this is my 2nd testcase

---
+++ src/reuse/MyNewTestcase.java
@@ -0,0 +1,20 @@
+package reuse;
+
+public class MyNewTestcase {
+
+	
+	public void main() {
+		System.out.println("this is my new one");
+	}
+	
+	public static void main(String[] args) {
+		MyNewTestcase my=new MyNewTestcase();
+		my.main();
+		
+		
+		
+		
+
+	}
+
+}
